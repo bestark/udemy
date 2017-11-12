0ae9745d137f9c3730fd6a007af8b6c599fbf736
Brian Stark <brian.joe.stark@gmail.com>
2017-11-12 10:35:37 +0100

    Launch

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+export class ServerComponent {
+
+  constructor() { }
+
+}
